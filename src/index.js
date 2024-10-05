7a3c49365dd793abeee44ac17999562d6efa5cd1
Torro15 <svetlgrish28@gmail.com>
2024-10-05 16:35:34 +0300

    git build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles/main.scss';
 
-
+// Скрипт для Слайдера
 import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
 
@@ -24,76 +24,49 @@ const swiper = new Swiper('.mySwiper', {
   },
 });
 
-// // Скрипт для меню с городами
-// document.addEventListener('DOMContentLoaded', () => {
-//   var menuIcon = document.getElementById('menu-icon');
-//   var dropdownMenu = document.getElementById('dropdown-menu');
-//   const overlay = document.querySelector('.overlay');
 
-//   menuIcon.addEventListener('click', () => {
-//     // Переключаем видимость меню и фона
-//     if (dropdownMenu.classList.contains('hidden')) {
-//       dropdownMenu.classList.remove('hidden');
-//       dropdownMenu.classList.add('visible');
-//       overlay.classList.add('visible'); // Показываем затемнение
-//     } else {
-//       dropdownMenu.classList.remove('visible');
-//       dropdownMenu.classList.add('hidden');
-//       overlay.classList.remove('visible'); // Скрываем затемнение
-//     }
-
-//     // Переключаем класс для поворота иконки
-//     menuIcon.classList.toggle('rotated');
-//   });
-
-//   // Закрытие меню при клике вне области
-//   overlay.addEventListener('click', () => {
-//     dropdownMenu.classList.remove('visible');
-//     dropdownMenu.classList.add('hidden');
-//     overlay.classList.remove('visible');
-//     menuIcon.classList.remove('rotated');
-//   });
-// });
+// Скрипт для меню с городами
 document.addEventListener('DOMContentLoaded', () => {
-const dropdownButton = document.getElementById('menu-icon');
-const desktopMenu = document.querySelector('.office__town');
-const mobileMenu = document.querySelector('.office__town-mobile');
-const overlay = document.querySelector('.overlay');
+  const dropdownButton = document.getElementById('menu-icon');
+  const desktopMenu = document.querySelector('.office__town');
+  const mobileMenu = document.querySelector('.office__town-mobile');
+  const overlay = document.querySelector('.overlay');
 
-dropdownButton.addEventListener('click', () => {
+  dropdownButton.addEventListener('click', () => {
     // Проверяем размер экрана
     if (window.innerWidth >= 768) {
-        // Десктопное меню
+      // Десктопное меню
+      if (desktopMenu.classList.contains('hidden')) {
         desktopMenu.classList.remove('hidden');
         desktopMenu.classList.add('visible');
-        overlay.classList.add('visible');
+        overlay.classList.add('visible'); // Показываем затемнение
+      } else {
+        desktopMenu.classList.remove('visible');
+        desktopMenu.classList.add('hidden');
+        overlay.classList.remove('visible'); // Скрываем затемнение
+      }
 
     } else {
-        // Мобильное меню
+      // Мобильное меню
+      if (mobileMenu.classList.contains('hidden')) {
         mobileMenu.classList.remove('hidden');
         mobileMenu.classList.add('visible');
-        overlay.classList.add('visible');    
+        overlay.classList.add('visible'); // Показываем затемнение
+      } else {
+        mobileMenu.classList.remove('visible');
+        mobileMenu.classList.add('hidden');
+        overlay.classList.remove('visible'); // Скрываем затемнение
+      }
     }
 
-    dropdownButton.classList.toggle('rotated'); 
+    dropdownButton.classList.toggle('rotated');
 
     overlay.addEventListener('click', () => {
-      desktopMenu.classList.remove('visible');
-      desktopMenu.classList.add('hidden');
-      mobileMenu.classList.remove('visible');
-      mobileMenu.classList.add('hidden');
-      overlay.classList.remove('visible');
+
       dropdownButton.classList.remove('rotated');
-      });
-
-    
-   });
-
-   
-
-  })
-  
-
+    });
+  });
+});
 
 
 // Скрипт для карт
@@ -127,49 +100,44 @@ document.addEventListener('DOMContentLoaded', () => {
 // Скрипт для блоков с направлениями
 const blocks = document.querySelectorAll('.expandableBlock');
 blocks.forEach(block => {
-    const toggleButton = block.querySelector('.icon-menu');
-    const additionalText = block.querySelector('.additionalText');
+  const toggleButton = block.querySelector('.icon-menu');
+  const additionalText = block.querySelector('.additionalText');
 
-    toggleButton.addEventListener('click', function () {
-        // Если блок развернут, свернуть его
-        if (block.style.height === '240px') {
-            block.style.height = '60px';
-            toggleButton.style.transform = 'rotate(0deg)';
-            additionalText.style.display = 'none';
-        } else {
-            // Развернуть блок
-            block.style.height = '240px';
-            toggleButton.style.transform = 'rotate(180deg)';
-            additionalText.style.display = 'block';
-        }
-    });
+  toggleButton.addEventListener('click', function () {
+    // Если блок развернут, свернуть его
+    if (block.style.height === '240px') {
+      block.style.height = '60px';
+      toggleButton.style.transform = 'rotate(0deg)';
+      additionalText.style.display = 'none';
+    } else {
+      // Развернуть блок
+      block.style.height = '240px';
+      toggleButton.style.transform = 'rotate(180deg)';
+      additionalText.style.display = 'block';
+    }
+  });
 });
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const menuIconMobile = document.getElementsByClassName('menu-icon-dropdown');
-//   const dropdownMenuMobile = document.getElementById('menu-dropdown');
-  
 
-//   menuIconMobile.addEventListener('click', () => {
-//     // Переключаем видимость меню и фона
-//     if (dropdownMenuMobile.classList.contains('hidden')) {
-//       dropdownMenuMobile.classList.remove('hidden');
-//       dropdownMenuMobile.classList.add('visible');
-      
-//     } else {
-//       dropdownMenuMobile.classList.remove('visible');
-//       dropdownMenuMobile.classList.add('hidden');
-      
-//     }
+// Скрипт для списков мобильного меню
+function toggleList(header) {
+  const content = header.nextElementSibling; // Получаем следующий элемент (список)
+  const button = header.querySelector('.button-mobile'); // Получаем кнопку
 
-//     // Переключаем класс для поворота иконки
-//     menuIconMobile.classList.toggle('rotated');
-//   });
+  // Переключаем активный класс для заглавной строки
+  header.classList.toggle('active');
 
-//   // Закрытие меню при клике вне области
-//   overlay.addEventListener('click', () => {
-//     dropdownMenuMobile.classList.remove('visible');
-//     dropdownMenuMobile.classList.add('hidden');
-//     menuIconMobile.classList.remove('rotated');
-//   });
-// });
+  // Переключаем активный класс для кнопки
+  button.classList.toggle('rotated');
+
+  // Переключаем видимость списка
+  if (content.classList.contains('visible')) {
+      content.classList.remove('visible'); // Скрыть список
+  } else {
+      content.classList.add('visible'); // Показать список
+  }
+}
+
+document.querySelectorAll('.office__town-main').forEach(header => {
+  header.addEventListener('click', () => toggleList(header));
+}); 
